5c556416a5c76b10eb94bf6fade9f3859808ff08
Anderson Silva de Jesus <andersondidot@hotmail.com>
2021-11-05 15:58:24 -0300

    Domain model, database seed

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
@@ -16,90 +16,74 @@ public class Sale {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private long id;
 	private Integer visited;
-	private Double amoute;
+	private Integer deals;
+	private Double amount;
 	private LocalDate date;
-
 	
 	@ManyToOne
 	@JoinColumn(name = "seller_id")
 	private Seller seller;
 	
-	
-	
 	public Sale() {
-		
-	}
+		}
 
-
-
-	public Sale(Long id, Integer visited, Double amoute, LocalDate date, Seller seller) {
+	public Sale(long id, Integer visited, Integer deals, Double amount, LocalDate date, Seller seller) {
 		this.id = id;
 		this.visited = visited;
-		this.amoute = amoute;
+		this.deals = deals;
+		this.amount = amount;
 		this.date = date;
 		this.seller = seller;
 	}
 
-
-
-	public Long getId() {
+	public long getId() {
 		return id;
 	}
 
-
-
-	public void setId(Long id) {
+	public void setId(long id) {
 		this.id = id;
 	}
-
-
 
 	public Integer getVisited() {
 		return visited;
 	}
 
-
-
 	public void setVisited(Integer visited) {
 		this.visited = visited;
 	}
 
-
-
-	public Double getAmoute() {
-		return amoute;
+	public Integer getDeals() {
+		return deals;
 	}
 
-
-
-	public void setAmoute(Double amoute) {
-		this.amoute = amoute;
+	public void setDeals(Integer deals) {
+		this.deals = deals;
 	}
 
+	public Double getAmount() {
+		return amount;
+	}
 
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
 
 	public LocalDate getDate() {
 		return date;
 	}
 
-
-
 	public void setDate(LocalDate date) {
 		this.date = date;
 	}
-
-
 
 	public Seller getSeller() {
 		return seller;
 	}
 
-
-
 	public void setSeller(Seller seller) {
 		this.seller = seller;
 	}
-	
+
 }
--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -16,26 +16,30 @@ public class Seller {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private long  id;
 	private String name;
 	
-	@OneToOne(mappedBy = "seller")
+	@OneToMany(mappedBy = "seller")
 	private List<Sale> sales = new ArrayList<>();
 	
-	public Seller() {
-		
+	public List<Sale> getSales() {
+		return sales;
 	}
 
-	public Seller(Long id, String name) {
+	public Seller() {
+		}
+
+	public Seller(long id, String name) {
+		super();
 		this.id = id;
 		this.name = name;
 	}
 
-	public Long getId() {
+	public long getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(long id) {
 		this.id = id;
 	}
 
@@ -46,10 +50,7 @@ public class Seller {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public List<Sale> getSales() {
-		return sales;
-	}
-
 	
+	
+
 }
